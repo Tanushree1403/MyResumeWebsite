2d4c87e29258abc84b2fa4d6d3c475930f9f5321
Tanushree <shreetanu979@gmail.com>
2019-11-08 15:47:21 -0800

    Adding Project Files

---
+++ Controllers/ProjectAvevaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyResumeWebsite.Controllers
+{
+    public class ProjectAvevaController : Controller
+    {
+        // GET: ProjectAveva
+        public ActionResult ProjectAveva()
+        {
+            return View();
+        }
+    }
+}
